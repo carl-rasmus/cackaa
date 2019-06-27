6523c0a79f9692738a64689158bfc54c5f63db60
Lars Marcus <vedelerlm@gmail.com>
2019-06-27 14:16:44 +0200

    root fix

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const express = require('express');
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/www'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
