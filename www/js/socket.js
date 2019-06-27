d46ea3b1b65af4cf025615177f2254adc597d3e9
Lars Marcus <vedelerlm@gmail.com>
2019-06-27 16:02:18 +0200

    socket work

---
+++ www/js/socket.js
@@ -0,0 +1,24 @@
+$(function() {
+  var socket = io();
+  var inputField = document.getElementById('m');
+  var messageField = document.getElementById('messages');
+  var chatItem = document.createElement('li');
+
+  $('form').submit(function(e){
+    e.preventDefault(); // prevents page reloading
+    socket.emit('chat message', inputField.value());
+    inputField.val('');
+    return false;
+  });
+  socket.on('chat message', function(msg){
+    messageField.appendChild(chatItem).text(msg);
+  });
+});
+
+
+    
+  function moveLid() {
+    var socket = io();
+    var lid = document.getElementById('lid');
+    lid.classList.add("lid-rotate");
+  }
