a7dab95e5226cec66e1c7da0fe2ec996deb1aaed
Lars Marcus <vedelerlm@gmail.com>
2019-06-27 16:04:17 +0200

    jqery revert

--- www/js/socket.js
+++ www/js/socket.js
@@ -1,22 +1,34 @@
-$(function() {
-  var socket = io();
-  var inputField = document.getElementById('m');
-  var messageField = document.getElementById('messages');
-  var chatItem = document.createElement('li');
+// $(function() {
+//   var socket = io();
+//   var inputField = document.getElementById('m');
+//   var messageField = document.getElementById('messages');
+//   var chatItem = document.createElement('li');
+//
+//   $('form').submit(function(e){
+//     e.preventDefault(); // prevents page reloading
+//     socket.emit('chat message', inputField.value());
+//     inputField.val('');
+//     return false;
+//   });
+//   socket.on('chat message', function(msg){
+//     messageField.appendChild(chatItem).text(msg);
+//   });
+// });
 
+$(function () {
+  var socket = io();
   $('form').submit(function(e){
     e.preventDefault(); // prevents page reloading
-    socket.emit('chat message', inputField.value());
-    inputField.val('');
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
     return false;
   });
   socket.on('chat message', function(msg){
-    messageField.appendChild(chatItem).text(msg);
+    $('#messages').append($('<li>').text(msg));
   });
 });
 
 
-    
   function moveLid() {
     var socket = io();
     var lid = document.getElementById('lid');
